8e6e810ea580a0aa266ed7ffd9788652841818b0
Asad Iqbal <official.asadiqbalnoor@gmail.com>
2025-07-16 23:16:31 +0500

    add routes in app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-const uri = "mongodb+srv://root:JZcqgPsuxtKM4V7y@cluster01.ykff7bw.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://root:JZcqgPsuxtKM4V7y@cluster01.ykff7bw.mongodb.net/?retryWrites=true&w=majority";
 
 // MongoDB connection
 mongoose
-  .connect(
-    uri,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("MongoDB Connection Error:", err));
 
+const urlRoutes = require("./routes/urlRoutes");
+app.use("/", urlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
